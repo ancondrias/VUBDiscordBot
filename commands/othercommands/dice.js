6c3528a00a8b07ea4726fd37780f7177d96e9d2a
Spieshe <dries.vandesteen@gmail.com>
2021-03-14 12:53:23 +0100

    Made it so dicerolls are serverspecific

--- commands/othercommands/dice.js
+++ commands/othercommands/dice.js
@@ -1,4 +1,4 @@
-const dicelog = [0]; 
+const multiServerLog = { 'default': 0 };
 
 // Importing module
 module.exports = {
@@ -7,12 +7,12 @@ module.exports = {
   description: "this is a dice command!",
   execute(Discord, message, args) {
 
-
+    const guildName = message.guild.name;
     // Embeding the response
     const embedDice = new Discord.MessageEmbed();
     embedDice.setColor('#FF6600')
-    .setAuthor(`Luigi's Cafe`, 'https://i.imgur.com/gmi8bjH.jpg', 'https://www.bruzz.be/luigis-cafe-2012-02-03')
-    .setThumbnail('https://i.imgur.com/gmi8bjH.jpg')
+      .setAuthor(`Luigi's Cafe`, 'https://i.imgur.com/gmi8bjH.jpg', 'https://www.bruzz.be/luigis-cafe-2012-02-03')
+      .setThumbnail('https://i.imgur.com/gmi8bjH.jpg')
 
 
     // Functions
@@ -21,11 +21,11 @@ module.exports = {
     var dice = die1 + die2;
 
 
-    console.log("\nMost recent old throw: " + dicelog[0]);
+    console.log("\nMost recent old throw: " + multiServerLog[guildName]);
     console.log(`You rolled: #${dice} [${die1}|${die2}]`);
     console.log("Arguments: " + args[0]);
 
-    embedDice.addFields({ name:`You rolled:`, value:`**#${dice}** [${die1}|${die2}]` });
+    embedDice.addFields({ name: `You rolled:`, value: `**#${dice}** [${die1}|${die2}]` });
 
     //conditions
     const guesses = ["higher", "hoger", "lower", "lager"];
@@ -34,15 +34,15 @@ module.exports = {
       embedDice.addFields({ name: `*Try guessing!*`, value: "*-vub higher* or *-vub lower*" });
     };
 
-    if (dicelog[0] === 0 && guesses.includes(args[0])) {
+    if (multiServerLog[guildName] === 0 && guesses.includes(args[0])) {
       embedDice.addFields({ name: `*You were the first to roll!*`, value: "Roll again or ask a friend" });
     };
 
-    if (dicelog[0] > 0 && guesses.includes(args[0])) {
-      if ((dice > dicelog[0] && (args[0] === "higher" || args[0] === "hoger")) || (dice < dicelog[0] && (args[1] === "lower" || args[0] === "lager"))) {
+    if (multiServerLog[guildName] > 0 && guesses.includes(args[0])) {
+      if ((dice > multiServerLog[guildName] && (args[0] === "higher" || args[0] === "hoger")) || (dice < multiServerLog[guildName] && (args[1] === "lower" || args[0] === "lager"))) {
         embedDice.addFields({ name: `*CORRECT GUESS*`, value: "You got lucky!" });
       }
-      else if (dice === dicelog[0]) {
+      else if (dice === multiServerLog[guildName]) {
         embedDice.addFields({ name: `YOU THREW THE SAME!`, value: "The person before you has to finish their glass!" });
       }
       else {
@@ -50,10 +50,8 @@ module.exports = {
       }
     }
 
-
-    dicelog.shift();
-    dicelog.push(dice);
-    console.log("New most recent old throw:" + dicelog[0]);
+    multiServerLog[guildName] = dice;
+    console.log("New most recent old throw:" + multiServerLog[guildName]);
 
     message.channel.send(embedDice);
     embedDice.fields = [];
